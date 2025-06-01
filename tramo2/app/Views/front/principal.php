2268ad7e21e9cdaf91e9a13af3a654a555776e9b
HardcoreFz <harcorefzgamerx@gmail.com>
2025-06-01 13:52:41 -0300

    transicion a tramo2

---
+++ tramo2/app/Views/front/principal.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>principal</title>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/miestilo.css">
+  </head>
+  <body>
+  <!--Si no comento esto el navbar,hay 2 navbar
+    <header>
+        <div class="logo-container">
+            <img src="Assets/img/yorha.png" alt="Logo de nier" class="logo">
+        </div>
+        <h1>For The Glory of Mankind</h1>
+    </header>
+
+    <nav>
+        <ul>
+            <li> <a href="home.html">Home</a></li>
+            <li><a href="nosotros.html">Quienes somos</a></li>
+            <li><a href="registro.html">Formulario de registro</a></li>
+        </ul>
+
+    </nav>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+
+-->
+    <section class="principal">
+
+    <div class="texto">
+        <p><strong>YoRHa is an elite military force of advanced androids designed to:</strong></p>
+        <p>combat the invading aliens and machine lifeforms. Operating out</p>
+        
+        <p>of their headquarters in the Bunker, YoRHa is led by Command</p>
+        <p>who receives orders from the Council of Humanity</p>
+        <p>YoRHa is also used to refer to the advanced android models</p>
+        
+        <p><em>themselves, sometimes referred to as YoRHa units</em></p>
+    </div>
+
+    
+
+    <!--Carrusel-->
+    <div id="carouselExample" class="carousel slide">
+        <div class="carousel-inner">
+        <div class="carousel-item active">
+        <img src="assets/img/yorha.png" class="d-block w-25 " alt="...">
+    </div>
+        <div class="carousel-item">
+        <img src="assets/img/type2b.webp" class="d-block w-25 " alt="type 2b">
+    </div>
+    <div class="carousel-item">
+        <img src="assets/img/type9s.webp" class="d-block w-25 " alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+<!--Fin carrusel-->
+  </section>
+ 
+
+  </body>
