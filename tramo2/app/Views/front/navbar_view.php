1d41d38c5298e745cd79f63ecd1e12e6adcf7aac
HardcoreFz <harcorefzgamerx@gmail.com>
2025-06-01 21:07:06 -0300

    Finalziacion del tramo2

--- tramo2/app/Views/front/navbar_view.php
+++ tramo2/app/Views/front/navbar_view.php
@@ -4,16 +4,15 @@
     
   <div class="container-fluid">
     <a class="navbar-brand" href="<?php echo base_url('/')?>">
-        <img src="<?php echo base_url('Assets/img/yorha.png')?>" alt="Logo de nier" width="75" height="30" class="img-fluid">
+        <img src="<?php echo base_url('Assets/img/yorha.png')?>" alt="Logo de nier" width="75" height="50" class="img-fluid">
+        <div class="text-center">Home</div>
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
+        
         <li class="nav-item">
           <a class="nav-link" href="quienes_somos">Quienes somos</a>
         </li>
--- tramo2/app/Views/front/principal.php
+++ tramo2/app/Views/front/principal.php
@@ -30,29 +30,28 @@
     <section class="principal">
 
     <div class="texto">
-        <p><strong>YoRHa is an elite military force of advanced androids designed to:</strong></p>
-        <p>combat the invading aliens and machine lifeforms. Operating out</p>
-        
-        <p>of their headquarters in the Bunker, YoRHa is led by Command</p>
-        <p>who receives orders from the Council of Humanity</p>
-        <p>YoRHa is also used to refer to the advanced android models</p>
-        
-        <p><em>themselves, sometimes referred to as YoRHa units</em></p>
+        <h1>Productos/Unidades</h1>
     </div>
 
     
 
     <!--Carrusel-->
-    <div id="carouselExample" class="carousel slide">
+    <div id="carouselExample" class="carousel slide" data-bs-theme="dark">
         <div class="carousel-inner">
-        <div class="carousel-item active">
-        <img src="assets/img/yorha.png" class="d-block w-25 " alt="...">
+        <div class="carousel-item active" >
+        <img src="assets/img/a2.webp" class="d-block w-25 " alt="A2">
+        <div class="carousel-text d-none d-md-block">
+          
+          <p>The A-model is a YoRHa prototype that specializes in close-quarters combat. Though not presently in use, it was originally created to speed along the implementation of other official models such as 2B and 9S.</p>
+        </div>
     </div>
         <div class="carousel-item">
         <img src="assets/img/type2b.webp" class="d-block w-25 " alt="type 2b">
+        <div class="carousel-text" d-none d-md-block>2B is an all-purpose battle android, deployed as a member of the automated YoRHa infantry. She is equipped with a multitude of weapons for close quarters combat and can attack from range using the Pod support system.</div>
     </div>
     <div class="carousel-item">
-        <img src="assets/img/type9s.webp" class="d-block w-25 " alt="...">
+        <img src="assets/img/type9s.webp" class="d-block w-25 " alt="9S">
+        <div class="carousel-text d-none d-md-block">9S is a scanner-type android who served during the 14th Machine War. He has an attack function, but is overall an investigative android—a member of the YoRHa automated infantry unit, which excels in hacking intelligence gathering.</div>
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
